a4f632bf1796075e8197d060d3a1dc9151a933e1
thewilkybarkid <chriswilkinson84@gmail.com>
2015-04-17 10:19:32 +0100

    Use Behaviors

--- js/teasers.js
+++ js/teasers.js
@@ -1,19 +1,23 @@
-jQuery(function ($) {
+(function ($) {
 
-    $('.view-content > .campl-row').each(function () {
-        var $teasers = $('.campl-teaser-border', this);
+    Drupal.behaviors.cambridgeTeasers = {
+        attach: function (context, settings) {
+            $('.view-content > .campl-row', context).each(function () {
+                var $teasers = $('.campl-teaser-border', this);
 
-        if (Modernizr.mq('only screen and (min-width: 768px)')) {
-            $teasers.matchHeight(false);
+                if (Modernizr.mq('only screen and (min-width: 768px)')) {
+                    $teasers.matchHeight(false);
+                }
+
+                $(window).resize(function () {
+                    if (Modernizr.mq('only screen and (max-width: 767px)')) {
+                        $teasers.height('auto');
+                    } else {
+                        $teasers.matchHeight(false);
+                    }
+                });
+            });
         }
+    };
 
-        $(window).resize(function () {
-            if (Modernizr.mq('only screen and (max-width: 767px)')) {
-                $teasers.height('auto');
-            } else {
-                $teasers.matchHeight(false);
-            }
-        });
-    });
-
-});
+})(jQuery);
