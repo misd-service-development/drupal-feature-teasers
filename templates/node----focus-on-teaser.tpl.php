30c11f398c0ad1017283f88a3d2ed1167bbf8f07
Tim Davison <tpd28@cam.ac.uk>
2020-10-09 20:17:15 +0100

    adjust focus on teaser template to only have a single link (or none)

--- templates/node----focus-on-teaser.tpl.php
+++ templates/node----focus-on-teaser.tpl.php
@@ -28,11 +28,16 @@ if (array_key_exists('field_link', $content)):
     $content['field_image'][0]['#path'] = array('path' => $url);
   endif;
 else:
-  $url = $node_url;
-  $read_more = t('Read more') . '<span class="element-invisible"> at: ' . $title . '</span>';
+  if ($type == 'link'):
+    $url = NULL;
+  else:
+    $url = $node_url;
+    $read_more = t('Read more') . '<span class="element-invisible"> at: ' . $title . '</span>';
+  endif;
 endif;
 
 $has_image = isset($content['field_image']);
+$has_url = isset($url);
 
 // We will have a single link for the teaser so ensure the image itself isn't linked
 if ($has_image):
@@ -43,6 +48,12 @@ endif;
 
 <div class="campl-content-container campl-side-padding <?php print $classes; ?>" <?php print $attributes; ?>>
   <div class="campl-horizontal-teaser campl-teaser clearfix campl-focus-teaser">
+
+    <?php if ($has_url): ?>
+      <a href="<?php print $url; ?>">
+      <span class="ir campl-focus-link"><?php print $read_more; ?></span>
+    <?php endif; ?>
+
     <div class="campl-focus-teaser-img">
       <div class="campl-content-container campl-horizontal-teaser-img">
         <?php if ($has_image): ?>
@@ -50,12 +61,16 @@ endif;
         <?php endif; ?>
       </div>
     </div>
+
+    <?php if ($has_url): ?>
+      </a>
+    <?php endif; ?>
+
     <div class="campl-focus-teaser-txt">
       <div class="campl-content-container campl-horizontal-teaser-txt">
         <?php print render($title_prefix); ?>
-        <h3 class="campl-teaser-title"><a href="<?php print $url; ?>"><?php print $title; ?></a></h3>
+        <h3 class="campl-teaser-title"><?php print $title; ?></h3>
         <?php print render($title_suffix); ?>
-        <a href="<?php print $url; ?>" class="ir campl-focus-link"><?php print $read_more; ?></a>
       </div>
     </div>
   </div>
