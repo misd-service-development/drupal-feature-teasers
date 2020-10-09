6f09e26b214d92333d1e6b54819926470d82e5aa
Tim Davison <tpd28@cam.ac.uk>
2020-10-09 14:41:03 +0100

    sort out getting data from field the proper way!

--- templates/node----news-listing-item.tpl.php
+++ templates/node----news-listing-item.tpl.php
@@ -1,15 +1,19 @@
 <?php
-
 hide($content['links']);
 
 if (array_key_exists('field_link', $content)):
   hide($content['field_link']);
+
+  // get field data
+  $field = field_get_items('node', $node, 'field_link');
+  $link_value = field_view_value('node', $node, 'field_link', $field[0]);
+
   // Build link with querystring and fragment
   $options = array (
-    'fragment' => $node->field_link[0]['fragment'],
-    'query'    => $node->field_link[0]['query'],
+    'fragment' => $link_value['#element']['fragment'],
+    'query'    => $link_value['#element']['query'],
   );
-  $url = url($node->field_link[0]['url'], $options);
+  $url = url($link_value['#element']['url'], $options);
 else:
   $url = $node_url;
 endif;
